165d96513f3aa3f34544d45fac7e2d7a26c34b9f
Snhvn <snhvnnguyen@gmail.com>
2025-03-28 13:18:06 +0700

    Create tool-install-tiktok.sh

---
+++ tool-install-tiktok.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+echo "______________________________________                       
+  / /    //\\     //   / /  /----\    /\
+/ /     //  \\   //   / /  /      /  /  \
+  / /  //   \\  //   / /  / __/  /  /____\
+/ /   //     \\//   / /  /         /      \
+______________________________________"
+echo "_______Youtube: https://youtube.com/@snipavn205_______"
+echo "_______Discord: https://dsc.gg/servertipacvn_______"
+echo "______________________________________"
+echo -e "Tiếp tục cài hay không, hãy nhập bất kì j vào trong ô nhập phía dưới"
+read ditiepk
+echo "Ok Bạn đã chọn đi tiếp"
+sleep 1
+echo "đi Thôi"
+sleep 2
+clear
+echo "______________________________________                       
+  / /    //\\     //   / /  /----\    /\
+/ /     //  \\   //   / /  /      /  /  \
+  / /  //   \\  //   / /  / __/  /  /____\
+/ /   //     \\//   / /  /         /      \
+______________________________________"
+echo "_______Youtube: https://youtube.com/@snipavn205_______"
+echo "_______Discord: https://dsc.gg/servertipacvn_______"
+echo "______________________________________"
+echo "Bạn muốn chọn tool nào?"
+echo "1. Tool TTp-Tool : nhập ttp-tool vào trong ô"
+echo "/_  __/_  __/ _ \
+       / /   / / / ___/
+      /_/   /_/ /_/"
+echo "Hãy nhập tên vào ô phía dưới 👇"
+read tool-name
+wget 
